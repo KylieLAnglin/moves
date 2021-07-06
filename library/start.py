77f6cc939d04aaa8d8dc88b01d661bcaba5c228f
KylieLAnglin <kylie@anglin.com>
2021-07-06 11:12:34 -0500

    import move checks

--- library/start.py
+++ library/start.py
@@ -1,10 +1,13 @@
-# HOME = "/Users/kylie/"
-HOME = "/Users/kylieanglin/"
+HOME = "/Users/kylie/"
+# HOME = "/Users/kylieanglin/"
 
 
-SHARED_PATH = (
-    HOME + "Box Sync/Measuring Coaching Fidelity and Quality/-coaching moves/"
+SHARED_PATH = HOME + "Box Sync/Measuring Coaching Fidelity and Quality/-coaching moves/"
+IRR_PATH = (
+    HOME
+    + "Box Sync/Measuring Coaching Fidelity and Quality/-coder training/Interrater Reliability/"
 )
+
 TRANSCRIPTS_PATH = SHARED_PATH + "uncoded transcripts/"
 
 DATA_PATH = HOME + "Dropbox/moves/data/"
